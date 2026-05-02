3fbbdfbe98b602ec57a00faba47f39ec4b2626c8
Abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2026-05-02 21:32:29 +0530

    Section-3: basic setup for typescript

---
+++ section-03/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `hello, ${name}`;
+}
+;
+console.log(greet('abhishek'));
+const username = "typescript-test-user";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ section-03/src/index.ts
@@ -0,0 +1,8 @@
+function greet(name: string): string{
+    return `hello, ${name}`;
+};
+
+console.log(greet('abhishek'));
+
+const username: string = "typescript-test-user";
+console.log(greet(username))
