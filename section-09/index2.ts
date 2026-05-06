f13cab1403a297234d81314a096dfba6181db93e
Abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2026-05-07 01:00:59 +0530

    Section-09: Pick Partial Required Ommit

--- section-09/index2.ts
+++ section-09/index2.ts
@@ -20,3 +20,61 @@ type Order = {
     items: Item[],
     address: Address
 }
+
+// --------------Pick, partial, required-------------------
+
+type Chai = {
+    name: string;
+    price: number;
+    isHot: boolean;
+}
+
+const updateChai = (updates: Partial<Chai>) => {
+    console.log("updating chai with", updates)
+}
+// we want to give value partially
+updateChai({price: 25})
+
+// can also pass empty function - can cause problems
+updateChai({isHot: false})
+
+
+// ------------------------- REQUIRED --------------------------
+type ChaiOrder = {
+    name?: string;
+    qunatity?: number
+}
+
+const placeOrder = (order: Required<ChaiOrder>) => {
+    console.log(order);
+}
+
+placeOrder({
+    name: "masala chai",
+    qunatity: 2
+})
+
+// ------------------------ Pick ---------------------------
+
+type chai = {
+    name: string;
+    price: number;
+    isHot: boolean;
+    ingredients: string[]
+}
+
+type BasicChaiInfo = Pick<chai, "name" | "price">;
+
+const chaiInfo: BasicChaiInfo = {
+    name: "lemon tea",
+    price: 30
+}
+
+type ChaiNew = {
+    name: string;
+    price: number;
+    isHot: boolean;
+    secretIngredients: string
+}
+
+type PublicChai = Omit<ChaiNew, "secretIngredients">
